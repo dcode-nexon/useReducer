5e73c01d214d92e0a24e377c562b28dba061c925
tubi55 <tubi55@nate.com>
2022-12-11 23:43:41 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'minus':
+			return state - action.payload;
+		case 'plus':
+			return state + action.payload;
+		default:
+			return state;
+	}
+};
+
 function App() {
-	return <main></main>;
+	const [number, dispatch] = useReducer(reducer, 0);
+
+	return (
+		<main>
+			<h1>{number}</h1>
+
+			<button onClick={() => dispatch({ type: 'minus', payload: 1 })}>minus</button>
+			<button onClick={() => dispatch({ type: 'plus', payload: 1 })}>plus</button>
+		</main>
+	);
 }
 
 export default App;
